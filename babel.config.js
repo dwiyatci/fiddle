467f6504e206f0e440aff81ec11f22127703dcd8
Glenn Dwiyatcita <glenn/dwiyatcita@sap.com>
2020-02-23 19:29:46 +0100

    ✨ feat: add some proposal-export-* related Babel plugin, polyfill runtime for proposals with corejs3 by default

--- babel.config.js
+++ babel.config.js
@@ -28,7 +28,7 @@ module.exports = function(api) {
       '@babel/plugin-transform-runtime',
       {
         absoluteRuntime: false,
-        corejs: 3,
+        corejs: { version: 3, proposals: true },
         helpers: true,
         regenerator: true,
         useESModules: false
@@ -36,6 +36,8 @@ module.exports = function(api) {
     ],
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
