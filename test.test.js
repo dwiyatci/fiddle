0df8d838b5185f3c6aba19af4be55a25426632f1
Glenn Dwiyatcita <glenn.dwiyatcita@sap.com>
2023-01-13 00:59:55 +0100

    jest cli testEnvironmentOptions

--- test.test.js
+++ test.test.js
@@ -25,17 +25,17 @@ describe('another test suite', () => {
   });
 });
 
-describe('a test suite', async () => {
-  const theAnswer = await Promise.resolve(42);
-
-  test('a test case', () => {
-    expect(theAnswer + 1).toEqual(43);
-  });
-
-  test('another test case', () => {
-    expect(theAnswer + -1).toEqual(41);
-  });
-});
+// describe('a test suite', async () => {
+//   const theAnswer = await Promise.resolve(42);
+//
+//   test('a test case', () => {
+//     expect(theAnswer + 1).toEqual(43);
+//   });
+//
+//   test('another test case', () => {
+//     expect(theAnswer + -1).toEqual(41);
+//   });
+// });
 
 // describe('another test suite', () => {
 //   let theAnswer;
@@ -51,3 +51,9 @@ describe('a test suite', async () => {
 //     expect(theAnswer + -1).toEqual(41);
 //   });
 // });
+
+test('testEnvironmentOptions', () => {
+  expect(global.theAnswer).toEqual(42);
+  expect(globalThis.theAnswer).toEqual(42);
+  expect(global.foo).toEqual('43');
+});
