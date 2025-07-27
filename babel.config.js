ebe463a6432ead7123e71cfad0d69d9efbcceb48
Glenn Dwiyatcita <glenn.dwiyatcita@sap.com>
2025-07-27 17:34:44 +0200

    📩 chore: downsizing things

--- babel.config.js
+++ babel.config.js
@@ -48,12 +48,7 @@ module.exports = function (api) {
         regenerator: true,
       },
     ],
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'hack', topicToken: '^^' }],
     ['client-only-require', { extensions: ['less', 'scss', 'css', 'png', 'gif', 'jpg'] }],
   ];
